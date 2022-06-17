868151bca4bb1ac648db0de6877b4e11255caa67
Viral Kanzaria <viralkanzaria@gmail.com>
2022-06-17 18:50:55 +0530

    Creating React Js Application

---
+++ react-beach-resort-dev/src/Components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Footer() {
+  const year = new Date().getFullYear();
+
+  return (
+    <footer className="footer">
+      <h2>Sunland Resorts | Hotel Room Book</h2>
+      
+    </footer>
+  );
+}
